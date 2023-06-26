a2c54cb3301ba680f1d07bc8254a383227d2736f
VOLODYMYR LYSENKO <107991551+Lusiy321@users.noreply.github.com>
2023-06-26 16:09:29 +0300

    upp

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -7,13 +7,21 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User extends Model<User> {
-  @ApiProperty({ example: 'Petro', description: 'User name' })
+  @ApiProperty({ example: 'Petro', description: 'User first name' })
   @Prop({
     type: String,
     minlength: 2,
     maxlength: 20,
   })
-  name: string;
+  firstName: string;
+
+  @ApiProperty({ example: 'Poroshenko', description: 'User last name' })
+  @Prop({
+    type: String,
+    minlength: 2,
+    maxlength: 20,
+  })
+  lastName: string;
 
   @ApiProperty({ example: 'poroshenko@gmail.com', description: 'User email' })
   @Prop({ type: String, required: [true, 'Email is required'] })
@@ -22,21 +30,64 @@ export class User extends Model<User> {
   @ApiProperty({ example: 'Petro-123545', description: 'User password' })
   @Prop({
     type: String,
-    minlength: 6,
+    minlength: 8,
     required: [true, 'Password is required'],
   })
   password: string;
 
+  @ApiProperty({
+    example: '+380987894556',
+    description: 'User phone number',
+    default: '+38000000000',
+  })
+  @Prop({
+    type: String,
+    minlength: 12,
+    maxlength: 12,
+  })
+  phone: string;
+
+  @ApiProperty({
+    example: 'Kyiv',
+    description: 'User location',
+    default: 'Kyiv',
+  })
+  @Prop({
+    type: String,
+    minlength: 2,
+    maxlength: 20,
+  })
+  location: string;
+
+  @ApiProperty({
+    example: 'https://',
+    description: 'User avatarURL',
+  })
+  @Prop({
+    type: String,
+  })
+  avatarURL: string;
+
   @ApiProperty({ example: 'admin', description: 'User role' })
-  @Prop({ required: true, enum: ['admin', 'boss', 'user'], default: 'user' })
+  @Prop({
+    required: true,
+    enum: ['admin', 'moderator', 'user'],
+    default: 'user',
+  })
   role: string;
+
+  @ApiProperty({ example: 'true', description: 'User status' })
+  @Prop({
+    type: Boolean,
+  })
+  isOnline: boolean;
 
   @ApiProperty({
     example: ['64849dc0b92d9a89e4bbf568'],
-    description: 'Subordinates users ID',
+    description: 'User post ID',
   })
   @Prop({ type: [{ type: 'ObjectId', ref: 'User' }], default: [] })
-  boss: User[];
+  postsId: Array<string>;
 
   @ApiProperty({
     example:
